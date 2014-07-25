8dd043267c8f1e8024c34e3e091aa750f8012fc3
newDivide <1063404489@qq.com>
2014-07-25 09:29:47 +0800

    small change

--- html/js/main.js
+++ html/js/main.js
@@ -6,3 +6,11 @@ function showBox(item){
 	document.getElementById("nav_"+item+"_concent").style.display="block";
 	document.getElementById("nav_"+item).className="select";   
 }
+function showButton(item){
+	for(var i=1;i<arguments.length;i++){
+		document.getElementById("nav_"+arguments[i]+"_content").style.display="none";
+		document.getElementById("nav_"+arguments[i]).className="no";
+	}
+	document.getElementById("nav_"+item+"_content").style.display="block";
+	document.getElementById("nav_"+item).className="select";  
+}
