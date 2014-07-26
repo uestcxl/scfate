110746acde494c9616d2368a8317187242fc1282
fhl <120308593@qq.com>
2014-07-26 21:52:41 +0800

    纪念品详细页面

--- protected/views/layouts/souvenir.php
+++ protected/views/layouts/souvenir.php
@@ -4,6 +4,10 @@
 ?>
 <head>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl?>/html/css/works.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl?>/html/css/souvenir.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl?>/html/css/dress_rent.css">
+	
+	<script type="text/javascript" src="<?php echo Yii::app()->baseUrl?>/html/js/main.js"></script>
 </head>
 <?php $this->beginContent('//layouts/main')?>
 	<!-- section  begin -->
@@ -11,14 +15,14 @@
 		<div class="ad">
 			<img src="<?php echo Yii::app()->baseUrl?>/html/img/AD.png">
 		</div>
-		<div class="works">
+		<div class="souvenir">
 			<div class="menu">
 				<div class="menu_part">
 					<div class="head"><h1>按品种分类</h1></div>
 					<div class="body">
-						<div><a href="<?php echo $this->createUrl('index');?>">所有作品</a></div>
+						<div class="menu_list"><a href="<?php echo $this->createUrl('index');?>">所有作品</a></div>
 						<?php if(!empty($sorts)) {foreach ($sorts as $key => $sort) {?>
-							<div><a href="<?php echo $this->createUrl('sort',array('sort'=>$sort->id,'type'=>'0'))?>"><?php echo $sort->sort_name;?></a></div>
+							<div class="menu_list"><a href="<?php echo $this->createUrl('sort',array('sort'=>$sort->id,'type'=>'0'))?>"><?php echo $sort->sort_name;?></a></div>
 						<?php }}?>
 					</div>
 				</div>
@@ -27,7 +31,7 @@
 					<div class="head"><h1>按学校分类</h1></div>
 					<div class="body">
 						<?php if (!empty($schools)) { foreach ($schools as $key => $school) {?>
-							<div><a href="<?php echo $this->createUrl('sort',array('sort'=>$school->id,'type'=>'1'))?>"><?php echo $school->area_name?></a></div>
+							<div class="menu_list"><a href="<?php echo $this->createUrl('sort',array('sort'=>$school->id,'type'=>'1'))?>"><?php echo $school->area_name?></a></div>
 						<?php }
 						}?>
 					</div>
--- protected/views/souvenir/index.php
+++ protected/views/souvenir/index.php
@@ -8,7 +8,7 @@
 							<img src="<?php echo Yii::app()->baseUrl.'/images/souvenir/'.$onesouvenir->picture?>">
 							<p>原价:<s><?php echo $onesouvenir->price;?>元</s></p>
 							<p>现价:<?php echo $onesouvenir->reduce_price;?>元</p>
-							<h2><?php echo $onesouvenir->name;?></h2>
+							<a href="<?php echo $this->createUrl('view',array('school_id'=>$onesouvenir->school_id,'id' =>$onesouvenir->id));?>"><?php echo $onesouvenir->name;?></a>
 						</div>
 					<?php }}?>
 				</div>
