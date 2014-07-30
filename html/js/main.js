36c600b992d1d0020b265ef34bde411ce19f3e98
newDivide <1063404489@qq.com>
2014-07-31 00:14:33 +0800

    index.html,rent_product_detail.html

--- html/js/main.js
+++ html/js/main.js
@@ -27,3 +27,62 @@ $(document).ready(function(){
 		$('.pop_delete').show();
 	});
 });
+$(document).ready(function(){  
+    var t = $("#text_box");  
+ 	
+    $("#add").click(function(){       
+        t.val(parseInt(t.val())+1)  
+       
+    })  
+    $("#min").click(function(){  
+       t.val(parseInt(t.val())-1)  
+    
+    }) ;
+    
+});
+$(document).ready(function(){
+		$('.model').each(function(){
+			var i=$(this);
+			var p=i.find('li');
+			p.focus(function(){
+				$(this).css('border','2px solid red');
+			});
+			p.click(function(){
+				if(!!$(this).hasClass('highlight')){
+					$(this).removeClass('highlight');
+				}else{
+					$(this).addClass('highlight').siblings('li').removeClass('highlight');
+					$(this).unbind('focus');
+				}
+
+				
+			});
+		});
+});
+$(document).ready(function(){
+
+	var cart = [];
+	
+
+	$('.shopping_cart').click(function(){
+		var goods = {};
+		goods.type = $(".button").find(".shopping_cart").attr("type");
+		goods.model = $(".model").find(".highlight a").text();
+		goods.num = parseInt($("#text_box").val(), 10);
+
+		if(cart.length != 0){
+			for(var i = 0; i < cart.length; i++){
+				if(cart[i].type == goods.type && cart[i].model == goods.model){
+					cart[i].num = cart[i].num + goods.num
+				}else {
+					goods.cid = ParseInt(Math.random() * 100000, 10);
+					cart.push(goods);
+				}
+			}
+		}else {
+			cart.push(goods);
+		}
+
+	})
+	
+})
